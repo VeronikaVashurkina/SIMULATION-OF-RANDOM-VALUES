43788db30c3a0970eb95887b004fcfa94051072c
Вероника Вашуркина <67638959+VeronikaVashurkina@users.noreply.github.com>
2021-06-07 23:09:41 +0300

    Add files via upload

---
+++ Lab1MIS/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Lab1MIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Частоты и длины интервалов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выборка:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(748, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Теоритические частоты:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(29, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 624);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(276, 82);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(367, 624);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(752, 82);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(265, 624);
+            this.listBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 731);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сгенерировать выборку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1227, 29);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Гистограмма частот:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 780);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1151, 26);
+            this.textBox1.TabIndex = 19;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(1231, 55);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Series2";
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(482, 358);
+            this.chart1.TabIndex = 20;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0,5",
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBox1.Location = new System.Drawing.Point(83, 734);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 28);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 737);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "a:";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(1231, 449);
+            this.chart2.Name = "chart2";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(482, 357);
+            this.chart2.TabIndex = 23;
+            this.chart2.Text = "chart2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1227, 416);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(194, 20);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Эмпирическая функция:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1747, 818);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab1MIS/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab1MIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int n = 200;
+            String b = (String)comboBox1.SelectedItem;
+          double a = double.Parse(b);
+
+            double[] x = new double[200];
+
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = (-1.0 / a) * Math.Log(rand.NextDouble());
+            }
+            Array.Sort(x);
+
+           
+            listBox1.Items.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                listBox1.Items.Add(x[i]);
+            }
+
+            
+            double length = x[n - 1] - x[0];
+            int N = 11;
+            double intervallength = length / N;
+
+            double[] xintervals = new double[N + 1];
+            xintervals[0] = x[0];
+            for (int i = 1; i < xintervals.Length; i++)
+            {
+                xintervals[i] = xintervals[i - 1] + intervallength;
+            }
+
+            
+            int ch = 0;
+            int[] chastota = new int[N+1];
+            for (int i = 0; i < N; i++)
+            {
+                ch = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (x[j] > xintervals[i] && x[j] <= xintervals[i + 1])
+                    {
+                        ch++;
+                    }
+                }
+                chastota[i] = ch; ;
+            }
+            
+
+
+           
+            listBox2.Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                listBox2.Items.Add(i + 1 + ")   " + chastota[i]);
+                listBox2.Items.Add(xintervals[i]);
+
+            }
+
+
+           
+
+          
+            double[] heights = new double[N];
+            for (int i = 0; i < N ; i++)
+            {
+                heights[i] = chastota[i] / (n * intervallength);
+            }
+
+            
+            double[] halfintervals = new double[N];
+            halfintervals[0] = x[0] + intervallength / 2.0;
+            for (int i = 1; i <N; i++)
+            {
+                halfintervals[i] = halfintervals[i - 1] + intervallength;
+            }
+
+            
+            this.chart1.Series.Clear();
+            Series series1 = this.chart1.Series.Add("Гистограмма частот");
+            Series series2 = this.chart1.Series.Add("Полигон частот");
+            chart1.Legends.Clear();
+            chart1.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            series1.ChartType = SeriesChartType.Column;
+            series1.Points.DataBindXY(halfintervals, heights);
+            series2.ChartType = SeriesChartType.Line;
+            series2.Points.DataBindXY(halfintervals, heights);
+
+            double[] Fx = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                double q = i;
+                Fx[i] = q / n;
+            }
+
+            this.chart2.Series.Clear();
+            Series series3 = this.chart2.Series.Add("Эмпирическая функция");
+            series3.ChartType = SeriesChartType.Line;
+            chart2.Legends.Clear();
+            chart2.ChartAreas[0].AxisX.Enabled = AxisEnabled.False;
+            series3.Points.DataBindXY(x, Fx);
+
+            
+            double[] chastotateor = new double[N];
+            double p1 = 0;
+            double p2 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                p1 = 1.0 - Math.Exp(-a * xintervals[i]);
+                p2 = 1.0 - Math.Exp(-a * xintervals[i + 1]);
+                chastotateor[i] = (p2 - p1) * n;
+                p1 = 0;
+                p2 = 0;
+            }
+
+            listBox3.Items.Clear();
+            for (int i = 0; i <N; i++)
+            {
+                listBox3.Items.Add(i + 1 + ") " + chastotateor[i]);
+            }
+
+            
+            double xi2 = 0;
+            for (int i = 0; i < N; i++)
+            {
+                xi2 += Math.Pow((double)chastota[i] - chastotateor[i], 2) / chastotateor[i];
+
+            }
+            if (xi2 < 23.2)
+            { textBox1.Text = "Критерий Пирсона в пределах нормы, равен:"+xi2+". Гипотеза об экспоненциальном распределении принимается."; }
+            else { textBox1.Text = "Критерий Пирсона  равен:" + xi2; }
+        }
+
+        
+    }
+}
